0ec3d7556216d466086bbb874dfcfe2d86a79ace
James Donohue <james.brackish@gmail.com>
2016-05-21 17:51:59 +0100

    Support options for envelope handling and add more tests

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 var fs = require('fs'),
     request = require('request-promise'),
-    makeBuilder = require('./lib/builder').makeBuilder,
+    createBuilder = require('./lib/builder').createBuilder,
     server = require('./lib/server'),
-    envelope = require('./lib/envelope');
+    Envelope = require('./lib/envelope')({ addLabels: true });
 
 const SERVER_PORT = 3000;
 
@@ -17,7 +17,7 @@ var readText = (filename) => fs.readFileSync(filename, 'utf-8'),
     readJson = (filename) => JSON.parse(readText(filename));
 
 var template = readText(process.argv[2]),
-    config = readJson(process.argv[3]),
+    config = readText(process.argv[3]),
     params = process.argv.length > 4 ? readJson(process.argv[4]) : {};
 
 // Optionally enable TLS certificate support
@@ -28,7 +28,7 @@ if (process.env.CERT_FILE && process.env.CA_FILE) {
     request = request.defaults({ cert: certData, key: certData, ca: caData });
 }
 
-const buildPage = makeBuilder(template, envelope, config, request, params);
+const buildPage = createBuilder(template, config, request, Envelope);
 
 server.start(buildPage, SERVER_PORT);
 
--- test/envelope.js
+++ test/envelope.js
@@ -1,8 +1,36 @@
-const assert = require('chai').assert,
-    envelope = require('../lib/envelope');
+const assert = require('chai').assert;
+const sinon = require('sinon');
+
+sinon.assert.expose(assert, { prefix: '' });
 
 describe('Envelope Utilities', function () {
+    describe('#parse', function () {
+        const body = '{ "bodyInline": "component body" }';
+        const component = { id: 'some-id' };
+
+        describe('with addLabels option not specified', function () {
+            const envelope = require('../lib/envelope')();
+
+            it('should parse JSON', function () {
+                assert.deepEqual(envelope.parse(body, component), { bodyInline: 'component body' });
+            });
+        });
+
+        describe('with addLabels option set to true', function () {
+            const envelope = require('../lib/envelope')({ addLabels: true });
+
+            it('should parse JSON and add component ID to bodyInline', function () {
+                const bodyInline = envelope.parse(body, component).bodyInline;
+
+                assert.match(bodyInline, /some-id/);
+                assert.match(bodyInline, /component body/);
+            });
+        });
+    });
+
     describe('#combine', function () {
+        const envelope = require('../lib/envelope')();
+
         it('should handle null or empty array input', function () {
             const emptyEnvelope = { head: '', bodyInline: '', bodyLast: '' };
 
@@ -59,6 +87,40 @@ describe('Envelope Utilities', function () {
             const result = envelope.combine(envelopes);
 
             assert.strictEqual(result.head, 'head1\n\nhead2');
+        });
+    });
+
+    describe('#recoverError', function () {
+        const component = { endpoint: 'http://internets.com/' };
+        let sandbox;
+
+        beforeEach(function () {
+            sandbox = sinon.sandbox.create();
+            sandbox.stub(console, 'error');
+        });
+
+        afterEach(function () {
+            sandbox.restore();
+        });
+
+        describe('with showErrors option not specified', function () {
+            const envelope = require('../lib/envelope')();
+
+            it('should return a failure message containing the URL', function () {
+                const bodyInline = envelope.recoverError(new Error(), component).bodyInline;
+
+                assert.match(bodyInline, /FAILED: http:\/\/internets.com\//);
+                assert.called(console.error);
+            });
+        });
+
+        describe('with showErrors option set to false', function () {
+            const envelope = require('../lib/envelope')({ showErrors: false });
+
+            it('should return an empty object', function () {
+                assert.deepEqual(envelope.recoverError(new Error(), component), {});
+                assert.called(console.error);
+            });
         });
     });
 });
