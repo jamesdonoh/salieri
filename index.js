97401b6bfcf3e7af572c99a7d046d0e6159bba43
James Donohue <james.brackish@gmail.com>
2016-05-22 11:11:44 +0100

    Parse options using yargs

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 'use strict';
 
-var fs = require('fs'),
-    request = require('request-promise'),
-    createBuilder = require('./lib/builder').createBuilder,
-    server = require('./lib/server'),
-    Envelope = require('./lib/envelope')({ addLabels: true });
+const readFile = (filename) => require('fs').readFileSync(filename, 'utf-8');
+let request = require('request-promise');
+
+const createBuilder = require('./lib/builder').createBuilder;
+const server = require('./lib/server');
+const Envelope = require('./lib/envelope')({ addLabels: true });
 
 const SERVER_PORT = 3000;
 
-if (process.argv.length < 4) {
-    console.log('Usage: node %s <template> <config> [params]', process.argv[1]);
-    process.exit(1);
+if (require.main === module) {
+    const argv = require('yargs')
+        .usage('Usage: $0 [options] -t <template> -c <config>')
+        .option('template', {
+            alias: 't',
+            demand: true,
+            describe: 'path to page template'
+        })
+        .option('config', {
+            alias: 'c',
+            demand: true,
+            describe: 'path to page config JSON'
+        })
+        .option('show-labels', {
+            describe: 'show component labels to help with debugging'
+        })
+        .option('hide-errors', {
+            describe: 'do not display request/parsing errors in markup'
+        })
+        .argv;
+
+    if (process.argv.length < 4) {
+        console.log('Usage: npm start <template> <config>');
+        process.exit(1);
+    }
+
+    const template = readFile(argv.template);
+    const config = readFile(argv.config);
+
+    // Optionally enable TLS certificate support
+    if (process.env.CERT_FILE && process.env.CA_FILE) {
+        let certData = readFile(process.env.CERT_FILE),
+            caData = readFile(process.env.CA_FILE);
+
+        request = request.defaults({ cert: certData, key: certData, ca: caData });
+    }
+
+    const buildPage = createBuilder(template, config, request, Envelope);
+
+    server.createServer(buildPage).listen(SERVER_PORT);
+
+    console.log('Listening on port %d', SERVER_PORT);
 }
-
-var readText = (filename) => fs.readFileSync(filename, 'utf-8'),
-    readJson = (filename) => JSON.parse(readText(filename));
-
-var template = readText(process.argv[2]),
-    config = readText(process.argv[3]),
-    params = process.argv.length > 4 ? readJson(process.argv[4]) : {};
-
-// Optionally enable TLS certificate support
-if (process.env.CERT_FILE && process.env.CA_FILE) {
-    let certData = fs.readFileSync(process.env.CERT_FILE),
-        caData = fs.readFileSync(process.env.CA_FILE);
-
-    request = request.defaults({ cert: certData, key: certData, ca: caData });
-}
-
-const buildPage = createBuilder(template, config, request, Envelope);
-
-server.createServer(buildPage).listen(SERVER_PORT);
-
-console.log('Listening on port %d', SERVER_PORT);
