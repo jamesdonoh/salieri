f88858f4d585fcc13e117f6f4818ebefdd21a684
James Donohue <james.brackish@gmail.com>
2016-05-25 22:04:29 +0100

    Integrate Envelope options into yargs parsing

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 'use strict';
 
 const readFile = (filename) => require('fs').readFileSync(filename, 'utf-8');
-let request = require('request-promise');
+let rp = require('request-promise');
 
 const createBuilder = require('./lib/builder').createBuilder;
 const server = require('./lib/server');
-const Envelope = require('./lib/envelope')({ addLabels: true });
 
 const SERVER_PORT = 3000;
 
-if (require.main === module) {
-    const argv = require('yargs')
-        .usage('Usage: $0 [options] -t <template> -c <config>')
-        .option('template', {
-            alias: 't',
-            demand: true,
-            describe: 'path to page template'
-        })
-        .option('config', {
-            alias: 'c',
-            demand: true,
-            describe: 'path to page config JSON'
-        })
-        .option('show-labels', {
-            describe: 'show component labels to help with debugging'
-        })
-        .option('hide-errors', {
-            describe: 'do not display request/parsing errors in markup'
-        })
-        .argv;
+const argv = require('yargs')
+    .usage('Usage: $0 [options] -t <template> -c <config>')
+    .option('template', {
+        alias: 't',
+        demand: true,
+        describe: 'path to page template'
+    })
+    .option('config', {
+        alias: 'c',
+        demand: true,
+        describe: 'path to page config JSON'
+    })
+    .option('cert', {
+        describe: 'send the specified client certificate to HTTPS servers'
+    })
+    .option('cacert', {
+        describe: 'use the specified CA certificate to verify HTTPS servers'
+    })
+    .option('labels', {
+        describe: 'show component labels to help with debugging',
+        default: false
+    })
+    .option('noerrors', {
+        describe: 'do not display request/parsing errors in markup',
+        default: false
+    })
+    .help('help')
+    .argv;
 
-    if (process.argv.length < 4) {
-        console.log('Usage: npm start <template> <config>');
-        process.exit(1);
-    }
+const template = readFile(argv.template);
+const config = readFile(argv.config);
 
-    const template = readFile(argv.template);
-    const config = readFile(argv.config);
-
-    // Optionally enable TLS certificate support
-    if (process.env.CERT_FILE && process.env.CA_FILE) {
-        let certData = readFile(process.env.CERT_FILE),
-            caData = readFile(process.env.CA_FILE);
-
-        request = request.defaults({ cert: certData, key: certData, ca: caData });
-    }
-
-    const buildPage = createBuilder(template, config, request, Envelope);
-
-    server.createServer(buildPage).listen(SERVER_PORT);
-
-    console.log('Listening on port %d', SERVER_PORT);
+if (argv.cert) {
+    const certData = readFile(argv.cert);
+    rp = rp.defaults({ cert: certData, key: certData });
 }
+
+if (argv.cacert) {
+    rp = rp.defaults({ ca: readFile(argv.cacert) });
+}
+
+const Envelope = require('./lib/envelope')(argv);
+
+const buildPage = createBuilder(template, config, rp, Envelope);
+
+server.createServer(buildPage)
+    .listen(SERVER_PORT, () => console.log('Listening on port %d', SERVER_PORT));
