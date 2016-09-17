23b982ddb4e05478fb6bc68246b8e8320b06a1e4
James Donohue <james.brackish@gmail.com>
2016-09-17 08:04:19 +0100

    Update tests

--- test/unit/handle-errors.js
+++ test/unit/handle-errors.js
@@ -1,90 +1,100 @@
 const chai = require('chai');
 const spies = require('chai-spies');
-
-const handleErrors = require('../../lib/handle-errors');
+const proxyquire = require('proxyquire').noCallThru();
 
 chai.use(spies);
 const expect = chai.expect;
 
 describe('Error handling', () => {
-    let req;
-    let app;
-
-    beforeEach(() => {
-        req = { app: { locals: { logError: chai.spy() } } };
-        next = chai.spy();
-    });
+    const envelopeUtilsStub = {
+        isValid: () => false
+    };
+    const stubs = { './envelopeUtils': envelopeUtilsStub };
 
     const mockRes = (...components) => ({ locals: { components: components } });
 
-    it('should call next', () => {
-        const res = { locals: { components: [] } };
+    let handleErrors;
+    let req;
+    let next;
 
-        handleErrors(req, res, next);
+    beforeEach(() => {
+        handleErrors = proxyquire('../../lib/handle-errors', stubs);
 
-        expect(next).to.have.been.called();
+        req = { app: { locals: { } } };
+        next = chai.spy('next');
     });
 
-    it('should mark components with errors as failed', () => {
-        const comp = { result: { error: { message: 'error message' } } };
-
-        handleErrors(req, mockRes(comp), next);
-
-        expect(comp.status).to.equal('failed');
-        expect(comp.reason).to.equal('error message');
+    afterEach(() => {
+        expect(next).to.have.been.called.once();
     });
 
-    it('should mark components with 202 responses as warnings', () => {
-        const comp = { result: { statusCode: 202 } };
-        const res = { locals: { components: [ comp ] } };
+    describe('unsuccessful envelopes', () => {
+        it('should mark components with errors as failed', () => {
+            const comp = { result: { error: { message: 'error message' } } };
 
-        handleErrors(req, mockRes(comp), next);
+            handleErrors(req, mockRes(comp), next);
 
-        expect(comp.status).to.equal('warning');
+            expect(comp.status).to.equal('failed');
+            expect(comp.reason).to.equal('error message');
+        });
+
+        it('should mark components with 202 responses as warnings', () => {
+            const comp = { result: { statusCode: 202 } };
+
+            handleErrors(req, mockRes(comp), next);
+
+            expect(comp.status).to.equal('warning');
+        });
+
+        it('should mark components with other non-200 response codes as errors', () => {
+            const comp = { result: { statusCode: 400 } };
+
+            handleErrors(req, mockRes(comp), next);
+
+            expect(comp.status).to.equal('failed');
+        });
+
+        it('should construct reason message using HTTP response', () => {
+            const comp = { result: { statusCode: 400, statusMessage: 'Bad request', body: 'Invalid parameters' } };
+
+            handleErrors(req, mockRes(comp), next);
+
+            expect(comp.reason).to.equal('400 Bad request - Invalid parameters');
+        });
+
+        it('should mark components with invalid envelopes responses as failures', () => {
+            const comp = { result: { statusCode: 200, body: 'hello' } };
+
+            handleErrors(req, mockRes(comp), next);
+
+            expect(comp.status).to.equal('failed');
+            expect(comp.reason).to.equal('Invalid envelope');
+        });
+
+        it('should log errors if logging enabled', () => {
+            const comp = { id: 'x', result: { error: { message: 'blah' } } };
+            req.app.locals.logError = chai.spy('logErrors');
+
+            handleErrors(req, mockRes(comp), next);
+
+            expect(req.app.locals.logError).to.have.been.called.with('x failed - blah');
+        });
+
+        it('in strict mode should invole error-handling middleware if component failed', () => {
+            const err = new Error('blah');
+            const comp = { id: 'y', must_succeed: true, result: { error: err } };
+            req.app.locals.strictMode = true;
+
+            handleErrors(req, mockRes(comp), next);
+
+            expect(next).to.have.been.called.with(err);
+        });
     });
 
-    it('should mark components with other non-200 response codes as errors', () => {
-        const comp = { result: { statusCode: 400 } };
-
-        handleErrors(req, mockRes(comp), next);
-
-        expect(comp.status).to.equal('failed');
-    });
-
-    it('should construct reason message using properties from HTTP response', () => {
-        const comp = { result: { statusCode: 400, statusMessage: 'Bad request', body: 'Invalid parameters' } };
-
-        handleErrors(req, mockRes(comp), next);
-
-        expect(comp.reason).to.equal('400 Bad request - Invalid parameters');
-    });
-
-    it('should mark components with non-JSON responses as failures', () => {
-        const comp = { result: { statusCode: 200, body: 'hello' } };
-
-        handleErrors(req, mockRes(comp), next);
-
-        expect(comp.status).to.equal('failed');
-        expect(comp.reason).to.equal('Invalid envelope');
-    });
-
-    it('should mark components with invalid envelopes as failures', () => {
-        const comp = { result: { statusCode: 200, body: { foo: 1 } } };
-
-        handleErrors(req, mockRes(comp), next);
-
-        expect(comp.status).to.equal('failed');
-    });
-
-    describe('compoments with valid envelopes', () => {
-        const validEnvelope = {
-            head: ['...', '...'],
-            bodyInline: 'ooo',
-            bodyLast: ['...']
-        };
-
-        it('should mark components as succeeded', () => {
-            const comp = { result: { statusCode: 200, body: validEnvelope } };
+    describe('successful envelopes', () => {
+        it('should mark components with valid envelopes as succeeded', () => {
+            const comp = { result: { statusCode: 200, body: 'valid' } };
+            envelopeUtilsStub.isValid = () => true;
 
             handleErrors(req, mockRes(comp), next);
 
@@ -92,11 +102,12 @@ describe('Error handling', () => {
         });
 
         it('should set envelope property to parsed envelope', () => {
-            const comp = { result: { statusCode: 200, body: validEnvelope } };
+            const envelope = { head: [], bodyInline: 'x' };
+            const comp = { result: { statusCode: 200, body: envelope } };
 
             handleErrors(req, mockRes(comp), next);
 
-            expect(comp.envelope).to.deep.equal(validEnvelope);
+            expect(comp.envelope).to.deep.equal(envelope);
         });
     });
 });
