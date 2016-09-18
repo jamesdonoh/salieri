9002d085af0d8a8e45c6f6f164bf705a871d5214
James Donohue <james.brackish@gmail.com>
2016-09-18 09:47:33 +0100

    Tests for prepareRequests

---
+++ test/unit/prepareRequests.spec.js
@@ -0,0 +1,46 @@
+const chai = require('chai');
+const spies = require('chai-spies');
+const proxyquire = require('proxyquire').noCallThru();
+
+chai.use(spies);
+const expect = chai.expect;
+
+describe('Preparing component requests', () => {
+    const stubs = {
+        'mustache': {}
+    };
+    
+    let prepareRequests;
+    let req;
+    let next;
+
+    beforeEach(() => {
+        prepareRequests = proxyquire('../../lib/prepareRequests', stubs);
+
+        req = { app: { locals: { } } };
+        res = { locals: { } };
+        next = chai.spy('next');
+    });
+
+    afterEach(() => {
+        expect(next).to.have.been.called.once();
+    });
+
+    it('should use Mustache to render config JSON using query parameters', () => {
+        req.app.locals.config = '{ "some": "config" }';
+        req.query = { param: 'some value' };
+        stubs.mustache.render = chai.spy((str) => str);
+
+        prepareRequests(req, res, next);
+
+        expect(stubs.mustache.render).to.have.been.called.with(req.app.locals.config, req.query);
+    });
+
+    it('should store prepared component data in response locals', () => {
+        stubs.mustache.render = (str) => '{ "components": [{ "a": "one"}, { "b": "two" }] }';
+
+        prepareRequests(req, res, next);
+
+        expect(res.locals.components).to.deep.equal([{ a: 'one' }, { b: 'two' }]);
+    });
+});
